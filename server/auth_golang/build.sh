c02f790401fbed498a7ad05d542905d5de226ea5
Franken Zeng <zengfanping@kingsoft.com>
2017-05-04 20:38:48 +0800

    update golang

--- server/auth_golang/build.sh
+++ server/auth_golang/build.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-set -x
-go build -o test test.go
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+CURRENT_DIR=`pwd`
+
+fmtsrc() {
+  for file in `find $1 -iregex '.*\.\(go\)$'`;do
+    if [[ "$file" =~ go$ ]];then
+      gofmt -w $file
+      echo $file
+    fi
+  done
+}
+
+case "$1" in
+format)
+  fmtsrc $CURRENT_DIR
+  ;;
+*)
+  set -x
+  go build -o test test.go
+esac
